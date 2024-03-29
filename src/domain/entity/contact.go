0c0efdb9d34c18e334bff9a65266dd0d9bb42595
ntorga <northontorga@gmail.com>
2024-03-29 11:44:51 -0300

    feat: add contact entity

---
+++ src/domain/entity/contact.go
@@ -0,0 +1,24 @@
+package entity
+
+import "github.com/ntorga/clean-ddd-go-taghs-poc-contacts/src/domain/valueObject"
+
+type Contact struct {
+	Id       valueObject.ContactId   `json:"id"`
+	Name     valueObject.PersonName  `json:"name"`
+	Nickname valueObject.Nickname    `json:"nickname"`
+	Phone    valueObject.PhoneNumber `json:"phone"`
+}
+
+func NewContact(
+	id valueObject.ContactId,
+	name valueObject.PersonName,
+	nickname valueObject.Nickname,
+	phone valueObject.PhoneNumber,
+) Contact {
+	return Contact{
+		Id:       id,
+		Name:     name,
+		Nickname: nickname,
+		Phone:    phone,
+	}
+}
