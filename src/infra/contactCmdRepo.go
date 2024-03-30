bc59f83d5c458d00b314d0a257c651e3e8abf54b
ntorga <northontorga@gmail.com>
2024-03-30 13:22:48 -0300

    feat: impl add contact infra

---
+++ src/infra/contactCmdRepo.go
@@ -0,0 +1,29 @@
+package infra
+
+import (
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/dto"
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/infra/db"
+	dbModel "github.com/ntorga/clean-ddd-taghs-poc-contacts/src/infra/db/model"
+)
+
+type ContactCmdRepo struct {
+	persistentDbSvc *db.PersistentDatabaseService
+}
+
+func NewContactCmdRepo(
+	persistentDbSvc *db.PersistentDatabaseService,
+) *ContactCmdRepo {
+	return &ContactCmdRepo{
+		persistentDbSvc: persistentDbSvc,
+	}
+}
+
+func (repo *ContactCmdRepo) Add(dto dto.AddContact) error {
+	model := dbModel.Contact{
+		Name:     dto.Name.String(),
+		Nickname: dto.Nickname.String(),
+		Phone:    dto.Phone.String(),
+	}
+
+	return repo.persistentDbSvc.Handler.Create(&model).Error
+}
