e382083248bd4a5909339d09e629c1b6c41d22c9
ntorga <northontorga@gmail.com>
2024-03-30 14:12:37 -0300

    feat: add delete contact domain layer

--- src/domain/repository/contactCmdRepo.go
+++ src/domain/repository/contactCmdRepo.go
@@ -1,8 +1,12 @@
 package repository
 
-import "github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/dto"
+import (
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/dto"
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/valueObject"
+)
 
 type ContactCmdRepo interface {
 	Create(dto dto.CreateContact) error
 	Update(dto dto.UpdateContact) error
+	Delete(id valueObject.ContactId) error
 }
---
+++ src/domain/useCase/deleteContact.go
@@ -0,0 +1,30 @@
+package useCase
+
+import (
+	"errors"
+	"log"
+
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/repository"
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/valueObject"
+)
+
+func DeleteContact(
+	contactQueryRepo repository.ContactQueryRepo,
+	contactCmdRepo repository.ContactCmdRepo,
+	id valueObject.ContactId,
+) error {
+	_, err := contactQueryRepo.ReadById(id)
+	if err != nil {
+		return errors.New("ContactNotFound")
+	}
+
+	err = contactCmdRepo.Delete(id)
+	if err != nil {
+		log.Printf("DeleteContactError: %s", err)
+		return errors.New("DeleteContactInfraError")
+	}
+
+	log.Printf("ContactId '%v' deleted.", id)
+
+	return nil
+}
