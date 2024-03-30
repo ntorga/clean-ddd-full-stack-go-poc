7adf5c8ae319b0fb4bdc594078eedded40581d88
ntorga <northontorga@gmail.com>
2024-03-30 13:00:54 -0300

    fix: request db svc on query repo

--- src/infra/contactQueryRepo.go
+++ src/infra/contactQueryRepo.go
@@ -1,12 +1,20 @@
 package infra
 
-import "github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/entity"
+import (
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/entity"
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/infra/db"
+)
 
 type ContactQueryRepo struct {
+	persistentDbSvc *db.PersistentDatabaseService
 }
 
-func NewContactQueryRepo() *ContactQueryRepo {
-	return &ContactQueryRepo{}
+func NewContactQueryRepo(
+	persistentDbSvc *db.PersistentDatabaseService,
+) *ContactQueryRepo {
+	return &ContactQueryRepo{
+		persistentDbSvc: persistentDbSvc,
+	}
 }
 
 func (repo *ContactQueryRepo) Get() ([]entity.Contact, error) {
