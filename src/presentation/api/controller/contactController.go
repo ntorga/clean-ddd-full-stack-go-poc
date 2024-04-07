bbc16b470eb6a9d3dc2cd407bdc73b94217c6fea
ntorga <northontorga@gmail.com>
2024-04-07 16:17:07 -0300

    refactor: use liaison on cli and api controllers

--- src/presentation/api/controller/contactController.go
+++ src/presentation/api/controller/contactController.go
@@ -1,26 +1,21 @@
 package apiController
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
-	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/dto"
-	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/useCase"
-	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/valueObject"
-	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/infra"
 	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/infra/db"
 	apiHelper "github.com/ntorga/clean-ddd-taghs-poc-contacts/src/presentation/api/helper"
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/presentation/liaison"
 )
 
 type ContactController struct {
-	persistentDbSvc *db.PersistentDatabaseService
+	contactLiaison *liaison.ContactLiaison
 }
 
 func NewContactController(
 	persistentDbSvc *db.PersistentDatabaseService,
 ) *ContactController {
 	return &ContactController{
-		persistentDbSvc: persistentDbSvc,
+		contactLiaison: liaison.NewContactLiaison(persistentDbSvc),
 	}
 }
 
@@ -33,13 +28,7 @@ func NewContactController(
 // @Success      200 {array} entity.Contact
 // @Router       /v1/contact/ [get]
 func (controller *ContactController) Read(c echo.Context) error {
-	contactsQueryRepo := infra.NewContactQueryRepo(controller.persistentDbSvc)
-	contactsList, err := useCase.ReadContacts(contactsQueryRepo)
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusInternalServerError, err.Error())
-	}
-
-	return apiHelper.ResponseWrapper(c, http.StatusOK, contactsList)
+	return apiHelper.ResponseWrapper(c, controller.contactLiaison.Read())
 }
 
 // CreateContact	 godoc
@@ -52,31 +41,15 @@ func (controller *ContactController) Read(c echo.Context) error {
 // @Success      201 {object} object{} "ContactCreated"
 // @Router       /v1/contact/ [post]
 func (controller *ContactController) Create(c echo.Context) error {
-	requiredParams := []string{"name", "nickname", "phone"}
-	requestBody, _ := apiHelper.ReadRequestBody(c)
-
-	apiHelper.CheckMissingParams(requestBody, requiredParams)
-
-	createContactDto := dto.NewCreateContact(
-		valueObject.NewPersonNamePanic(requestBody["name"].(string)),
-		valueObject.NewNicknamePanic(requestBody["nickname"].(string)),
-		valueObject.NewPhoneNumberPanic(requestBody["phone"].(string)),
-	)
-
-	persistentDbSvc := c.Get("persistentDbSvc").(*db.PersistentDatabaseService)
-	contactQueryRepo := infra.NewContactQueryRepo(persistentDbSvc)
-	contactCmdRepo := infra.NewContactCmdRepo(persistentDbSvc)
-
-	err := useCase.CreateContact(
-		contactQueryRepo,
-		contactCmdRepo,
-		createContactDto,
-	)
+	requestBody, err := apiHelper.ReadRequestBody(c)
 	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
+		return err
 	}
 
-	return apiHelper.ResponseWrapper(c, http.StatusCreated, "ContactCreated")
+	return apiHelper.ResponseWrapper(
+		c,
+		controller.contactLiaison.Create(requestBody),
+	)
 }
 
 // UpdateContact godoc
@@ -89,54 +62,15 @@ func (controller *ContactController) Create(c echo.Context) error {
 // @Success      200 {object} object{} "ContactUpdated"
 // @Router       /v1/contact/ [put]
 func (controller *ContactController) Update(c echo.Context) error {
-	requiredParams := []string{"id"}
-	requestBody, _ := apiHelper.ReadRequestBody(c)
-
-	apiHelper.CheckMissingParams(requestBody, requiredParams)
-
-	id := valueObject.NewContactIdPanic(requestBody["id"])
-
-	var namePtr *valueObject.PersonName
-	if requestBody["name"] != nil {
-		name := valueObject.NewPersonNamePanic(requestBody["name"].(string))
-		namePtr = &name
-	}
-
-	var nickNamePtr *valueObject.Nickname
-	if requestBody["nickname"] != nil {
-		nickname := valueObject.NewNicknamePanic(requestBody["nickname"].(string))
-		nickNamePtr = &nickname
-	}
-
-	var phonePtr *valueObject.PhoneNumber
-	if requestBody["phone"] != nil {
-		phone := valueObject.NewPhoneNumberPanic(requestBody["phone"].(string))
-		phonePtr = &phone
-	}
-
-	updateContactDto := dto.NewUpdateContact(
-		id,
-		namePtr,
-		nickNamePtr,
-		phonePtr,
-	)
-
-	persistentDbSvc := c.Get("persistentDbSvc").(*db.PersistentDatabaseService)
-	contactQueryRepo := infra.NewContactQueryRepo(persistentDbSvc)
-	contactCmdRepo := infra.NewContactCmdRepo(persistentDbSvc)
-
-	err := useCase.UpdateContact(
-		contactQueryRepo,
-		contactCmdRepo,
-		updateContactDto,
-	)
+	requestBody, err := apiHelper.ReadRequestBody(c)
 	if err != nil {
-		return apiHelper.ResponseWrapper(
-			c, http.StatusInternalServerError, err.Error(),
-		)
+		return err
 	}
 
-	return apiHelper.ResponseWrapper(c, http.StatusOK, "ContactUpdated")
+	return apiHelper.ResponseWrapper(
+		c,
+		controller.contactLiaison.Update(requestBody),
+	)
 }
 
 // DeleteContact godoc
@@ -149,20 +83,12 @@ func (controller *ContactController) Update(c echo.Context) error {
 // @Success      200 {object} object{} "ContactDeleted"
 // @Router       /v1/contact/{id}/ [delete]
 func (controller *ContactController) Delete(c echo.Context) error {
-	id := valueObject.NewContactIdPanic(c.Param("id"))
-
-	persistentDbSvc := c.Get("persistentDbSvc").(*db.PersistentDatabaseService)
-	contactQueryRepo := infra.NewContactQueryRepo(persistentDbSvc)
-	contactCmdRepo := infra.NewContactCmdRepo(persistentDbSvc)
-
-	err := useCase.DeleteContact(
-		contactQueryRepo,
-		contactCmdRepo,
-		id,
-	)
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
+	requestBody := map[string]interface{}{
+		"id": c.Param("id"),
 	}
 
-	return apiHelper.ResponseWrapper(c, http.StatusOK, "ContactDeleted")
+	return apiHelper.ResponseWrapper(
+		c,
+		controller.contactLiaison.Delete(requestBody),
+	)
 }
--- src/presentation/cli/controller/contactController.go
+++ src/presentation/cli/controller/contactController.go
@@ -1,24 +1,21 @@
 package cliController
 
 import (
-	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/dto"
-	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/useCase"
-	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/domain/valueObject"
-	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/infra"
 	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/infra/db"
 	cliHelper "github.com/ntorga/clean-ddd-taghs-poc-contacts/src/presentation/cli/helper"
+	"github.com/ntorga/clean-ddd-taghs-poc-contacts/src/presentation/liaison"
 	"github.com/spf13/cobra"
 )
 
 type ContactController struct {
-	persistentDbSvc *db.PersistentDatabaseService
+	contactLiaison *liaison.ContactLiaison
 }
 
 func NewContactController(
 	persistentDbSvc *db.PersistentDatabaseService,
 ) *ContactController {
 	return &ContactController{
-		persistentDbSvc: persistentDbSvc,
+		contactLiaison: liaison.NewContactLiaison(persistentDbSvc),
 	}
 }
 
@@ -27,13 +24,7 @@ func (controller *ContactController) Read() *cobra.Command {
 		Use:   "read",
 		Short: "ReadContacts",
 		Run: func(cmd *cobra.Command, args []string) {
-			contactQueryRepo := infra.NewContactQueryRepo(controller.persistentDbSvc)
-			contactsList, err := useCase.ReadContacts(contactQueryRepo)
-			if err != nil {
-				cliHelper.ResponseWrapper(false, err.Error())
-			}
-
-			cliHelper.ResponseWrapper(true, contactsList)
+			cliHelper.ResponseWrapper(controller.contactLiaison.Read())
 		},
 	}
 
@@ -49,29 +40,13 @@ func (controller *ContactController) Create() *cobra.Command {
 		Use:   "create",
 		Short: "CreateNewContact",
 		Run: func(cmd *cobra.Command, args []string) {
-			name := valueObject.NewPersonNamePanic(nameStr)
-			nickname := valueObject.NewNicknamePanic(nicknameStr)
-			phone := valueObject.NewPhoneNumberPanic(phoneStr)
-
-			createContactDto := dto.NewCreateContact(
-				name,
-				nickname,
-				phone,
-			)
-
-			contactQueryRepo := infra.NewContactQueryRepo(controller.persistentDbSvc)
-			contactCmdRepo := infra.NewContactCmdRepo(controller.persistentDbSvc)
-
-			err := useCase.CreateContact(
-				contactQueryRepo,
-				contactCmdRepo,
-				createContactDto,
-			)
-			if err != nil {
-				cliHelper.ResponseWrapper(false, err.Error())
+			requestBody := map[string]interface{}{
+				"name":     nameStr,
+				"nickname": nicknameStr,
+				"phone":    phoneStr,
 			}
 
-			cliHelper.ResponseWrapper(true, "ContactCreated")
+			cliHelper.ResponseWrapper(controller.contactLiaison.Create(requestBody))
 		},
 	}
 
@@ -94,46 +69,14 @@ func (controller *ContactController) Update() *cobra.Command {
 		Use:   "update",
 		Short: "UpdateContact",
 		Run: func(cmd *cobra.Command, args []string) {
-			id := valueObject.NewContactIdPanic(idStr)
-
-			var namePtr *valueObject.PersonName
-			if nameStr != "" {
-				name := valueObject.NewPersonNamePanic(nameStr)
-				namePtr = &name
+			requestBody := map[string]interface{}{
+				"id":       idStr,
+				"name":     nameStr,
+				"nickname": nicknameStr,
+				"phone":    phoneStr,
 			}
 
-			var nicknamePtr *valueObject.Nickname
-			if nicknameStr != "" {
-				nickname := valueObject.NewNicknamePanic(nicknameStr)
-				nicknamePtr = &nickname
-			}
-
-			var phonePtr *valueObject.PhoneNumber
-			if phoneStr != "" {
-				phone := valueObject.NewPhoneNumberPanic(phoneStr)
-				phonePtr = &phone
-			}
-
-			updateContactDto := dto.NewUpdateContact(
-				id,
-				namePtr,
-				nicknamePtr,
-				phonePtr,
-			)
-
-			contactQueryRepo := infra.NewContactQueryRepo(controller.persistentDbSvc)
-			contactCmdRepo := infra.NewContactCmdRepo(controller.persistentDbSvc)
-
-			err := useCase.UpdateContact(
-				contactQueryRepo,
-				contactCmdRepo,
-				updateContactDto,
-			)
-			if err != nil {
-				cliHelper.ResponseWrapper(false, err.Error())
-			}
-
-			cliHelper.ResponseWrapper(true, "ContactUpdated")
+			cliHelper.ResponseWrapper(controller.contactLiaison.Update(requestBody))
 		},
 	}
 
@@ -152,21 +95,11 @@ func (controller *ContactController) Delete() *cobra.Command {
 		Use:   "delete",
 		Short: "DeleteContact",
 		Run: func(cmd *cobra.Command, args []string) {
-			id := valueObject.NewContactIdPanic(idStr)
-
-			contactQueryRepo := infra.NewContactQueryRepo(controller.persistentDbSvc)
-			contactCmdRepo := infra.NewContactCmdRepo(controller.persistentDbSvc)
-
-			err := useCase.DeleteContact(
-				contactQueryRepo,
-				contactCmdRepo,
-				id,
-			)
-			if err != nil {
-				cliHelper.ResponseWrapper(false, err.Error())
+			requestBody := map[string]interface{}{
+				"id": idStr,
 			}
 
-			cliHelper.ResponseWrapper(true, "ContactDeleted")
+			cliHelper.ResponseWrapper(controller.contactLiaison.Delete(requestBody))
 		},
 	}
 
