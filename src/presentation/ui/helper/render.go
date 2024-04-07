a81003b1e10c8803f5f9c3aeba5dc71e51f989af
ntorga <northontorga@gmail.com>
2024-04-07 11:21:56 -0300

    feat: add render ui helper

---
+++ src/presentation/ui/helper/render.go
@@ -0,0 +1,12 @@
+package uiHelper
+
+import (
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+)
+
+func Render(c echo.Context, component templ.Component) error {
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+
+	return component.Render(c.Request().Context(), c.Response())
+}
