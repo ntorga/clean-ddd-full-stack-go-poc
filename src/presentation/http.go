1c5d50f6e844f8b31b5e013c01521f8c0269308d
ntorga <northontorga@gmail.com>
2024-05-01 18:44:03 -0300

    feat: add custom server banner

--- src/presentation/http.go
+++ src/presentation/http.go
@@ -1,6 +1,8 @@
 package presentation
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo/v4"
 	"github.com/ntorga/clean-ddd-full-stack-go-poc/src/infra/db"
 	"github.com/ntorga/clean-ddd-full-stack-go-poc/src/presentation/api"
@@ -13,5 +15,17 @@ func HttpServerInit(persistentDbSvc *db.PersistentDatabaseService) {
 	api.ApiInit(e, persistentDbSvc)
 	ui.UiInit(e, persistentDbSvc)
 
+	e.HideBanner = true
+
+	serverBanner := `
+░█▀▀░█░░░█▀▀░█▀█░█▀█░░░█▀▄░█▀▄░█▀▄              
+░█░░░█░░░█▀▀░█▀█░█░█░░░█░█░█░█░█░█        v0.0.1
+░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀░░░▀▀░░▀▀░░▀▀░              
+░█▀▀░█░█░█░░░█░░░░░█▀▀░▀█▀░█▀█░█▀▀░█░█░░░█▀▀░█▀█
+░█▀▀░█░█░█░░░█░░░░░▀▀█░░█░░█▀█░█░░░█▀▄░░░█░█░█░█
+░▀░░░▀▀▀░▀▀▀░▀▀▀░░░▀▀▀░░▀░░▀░▀░▀▀▀░▀░▀░░░▀▀▀░▀▀▀
+`
+
+	fmt.Println(serverBanner)
 	e.Start(":8080")
 }
